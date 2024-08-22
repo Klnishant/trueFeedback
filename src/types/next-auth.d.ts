8698feabd688e6f2411035138ac707a4bfa0e7a4
Klnishant <nishantkaushal075@gmail.com>
2024-08-22 16:51:52 +0530

    create auth options and bug fixes

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,7 +2,7 @@ import 'next-auth'
 import { DefaultSession } from 'next-auth'
 
 declare module 'next-auth' {
-    interface session{
+    interface Session{
         user: {
             _id?: string;
             isVerified?: boolean;

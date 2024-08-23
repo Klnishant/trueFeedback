6d9e553fca909a2bf13db33a86673b84e898fcd8
Klnishant <nishantkaushal075@gmail.com>
2024-08-23 11:57:44 +0530

    create route for check unique username

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,80 @@
+import dbconnect from "@/lib/dbconnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { URL } from "next/dist/compiled/@edge-runtime/primitives/url";
+
+const usernameQuerySchema = z.object({
+    username: usernameValidation,
+});
+
+export async function GET (request: Request){
+    await dbconnect();
+
+    try {
+        const {searchParams} = new URL(request.url);
+        const queryParams = {
+            username: searchParams.get('username')
+        };
+
+        const result = usernameQuerySchema.safeParse(queryParams);
+
+        if (!result.success) {
+            const usernameError = result.error.format().username?._errors || [];
+
+            return Response.json(
+                {
+                    success: false,
+                    message: 
+                        usernameError?.length>0
+                        ?usernameError.join(', ')
+                        :'invalid query parameters',
+                },
+                {
+                    status:400,
+                },
+            );
+        }
+
+        const {username} = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({
+            username,
+            isverified: true,
+        });
+
+        if (existingVerifiedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: 'Username already taken'
+                },
+                {
+                    status:400,
+                },
+            );
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: 'username is unique',
+            },
+            {
+                status:200,
+            },
+        );
+    } catch (error) {
+        console.error('Erroe checking username',error);
+
+        return Response.json(
+            {
+                success: false,
+                message: 'Error checking username',
+            },
+            {
+                status: 500,
+            },
+        );
+    }
+}
